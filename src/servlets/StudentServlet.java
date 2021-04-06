87840f36880fc99eca46d13d18335a2e0d5a38dc
turab45 <turabbajeer202@gmail.com>
2021-04-06 15:40:33 +0500

    Add student registration

--- src/servlets/StudentServlet.java
+++ src/servlets/StudentServlet.java
@@ -7,32 +7,71 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.StudentDao;
+import daoimpl.StudentDaoImpl;
+import models.Student;
+
 /**
  * Servlet implementation class StudentServlet
  */
 @WebServlet("/Student")
 public class StudentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public StudentServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	
+	StudentDao studentDaoImpl = new StudentDaoImpl();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public StudentServlet() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		
+		System.out.println("Action  "+action);
+
+		switch (action) {
+		case "create":
+			
+			Student student = new Student();
+			
+			student.setName(request.getParameter("name"));
+			student.setEmail(request.getParameter("email"));
+			student.setContact(request.getParameter("contact"));
+			student.setGender(request.getParameter("gender"));
+			student.setCountry(request.getParameter("country"));
+			
+			Integer result = studentDaoImpl.addStudent(student);
+			
+			if (result > 0) {
+				response.getWriter().write("Student added successfully.");
+			}
+
+			break;
+		case "update":
+
+			break;
+		case "delete":
+
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
